6bbf8bf484b092f8a2404416f3d5025a2cedf743
Radisav <rades12340@gmail.com>
2020-02-13 17:24:56 +0100

    cors added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const db = require("./database/db");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
